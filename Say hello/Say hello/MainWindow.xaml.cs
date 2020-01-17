1afaaf7619c17f311aa48812bf8139e24d0a4b1d
InvokeReal <56397847+InvokeReal@users.noreply.github.com>
2020-01-17 20:19:56 +0400

    Add files via upload

---
+++ Say hello/Say hello/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Say_hello
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand;
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            //  Fill an array of bytes of length "n" with random numbers.
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+        }
+    }
+}
