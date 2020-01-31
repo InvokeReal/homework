65b5ac379c09d1cbf2462d7e50748425a4830d73
InvokeReal <56397847+InvokeReal@users.noreply.github.com>
2020-01-31 22:03:27 +0400

    Update MainWindow.xaml.cs

--- WpfAppPractika/WpfAppPractika/MainWindow.xaml.cs
+++ WpfAppPractika/WpfAppPractika/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfAppPractika
+namespace CalculateWPF
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -42,8 +42,64 @@ namespace WpfAppPractika
             String s = (string)((Button)e.OriginalSourse).Content;
             textBlock.Text += s;
             int num;
-            bool result = Int32.TryParse(s,)
+            bool result = Int32.TryParse(s, out num);
+            if (result == true)
+            {
+              if (operation == "")
+              {
+                leftop += s;
+              }
+              else
+              {
+              rightop += s;
+              }
+         }
+         else
+         { 
+         if (s == "=")
+         {
+         Update_RightOp();
+         textBlock.Text += rightop;
+         operation = "";
+         }
+         else if (s== "CLEAR")
+         {
+           leftop= "";
+           rightop= "";
+           operation="";
+           textBlock.Text= "";
+           }
+           else
+           {
+             if (rightop != "")
+             {
+               Update_RigtOp();
+               leftop = rightop;
+               rightop= "";
+               }
+               operation = s;
         }
-        
     }
 }
+private void Update_RightOp()
+{ 
+int num1= Int32.Parse(leftop);
+int num2= Int32.Parse(rightop);
+switch (operation)
+{
+case "+":
+rightop = (num1 + num2).ToString();
+break;
+case "-:
+rightop = (num1 + num2).ToString();
+break;
+case "*"
+rightop = (num1 + num2).ToString();
+break;
+case "/:
+rightop = (num1 + num2).ToString();
+break;
+    }
+  }
+}
+
